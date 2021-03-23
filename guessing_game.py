7b166db577cba4656caee5ec9458f4b1686e18e4
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-03-23 09:40:07 +0100

    Finish basic expectations.

--- guessing_game.py
+++ guessing_game.py
@@ -17,15 +17,15 @@ def start_game():
     
     When the program starts, we want to:
     ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
+    1. Display an intro/welcome message to the player: DONE
+    2. Store a random number as the answer/solution: DONE
     3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
+      a. If the guess greater than the solution, display to the player "It's lower": DONE
+      b. If the guess is less than the solution, display to the player "It's higher": DONE
     
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
+    4. Once the guess is correct, stop looping, inform the user they "Got it": DONE
+         and show how many attempts it took them to get the correct number: DONE
+    5. Let the player know the game is ending, or something that indicates the game is over: DONE
     
     ( You can add more features/enhancements if you'd like to. )
     """
@@ -34,11 +34,30 @@ def start_game():
     print("Welcome to the Number Guessing Game!")
     print("------------------------------------")
 
-    number_proposal = int(input("Pick a number between 1 and 10: "))
+    random_number = random.randint(1, 10)
+    number_proposal = 0
+    number_of_attempts = 0
 
-    print("You have picked {}".format(number_proposal))
+    print("======")
+    print("random_number", random_number)
+    print("======")
+    print("======")
+    print("number_proposal", number_proposal)
+    print("======")
+    
+    while random_number != number_proposal:
+      number_proposal = int(input("Pick a number between 1 and 10: "))
 
+      if number_proposal < random_number:
+        print("It is higher")
+      elif number_proposal > random_number:
+        print("It is lower")
 
+      number_of_attempts += 1
+    
+
+    print("Got it! It took you {} tries to find out the correct answer".format(number_of_attempts))
+    print("Closing game, see you next time")
 
 # Kick off the program by calling the start_game function.
 start_game()
